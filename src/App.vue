22e212deb37cba274bd03d370116bd562c461bad
streakingman <z_max_y@163.com>
2021-01-09 22:07:37 +0800

    chore: temp

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
     <logo msg="Hello Vue 3.0 + Vite"/>
     <search></search>
+    <div></div>
     <link-card label="画图用的" url="//www.draw.io"></link-card>
     <link-card label="粤哥博客" url="//streakingman.github.io"></link-card>
     <link-card label="晋江" url="//http://my.jjwxc.net/"></link-card>
